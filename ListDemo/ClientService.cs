c8e16fa767d4150702c52bfc9f5729c38652fda3
Björn Bergdahl <84767875+BearBergdahl@users.noreply.github.com>
2023-10-25 12:37:02 +0200

    Add project files.

---
+++ ListDemo/ClientService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using static System.Net.WebRequestMethods;
+
+namespace DemoClient
+{
+    public class ClientService
+    {
+        public RestResponse GetInsult()
+        {
+            RestClient client = CreateClient($"https://evilinsult.com/generate_insult.php?lang=en&type=json");
+            return MakeRestCall(client);
+        }
+
+        private RestResponse MakeRestCall(RestClient restClient)
+        {
+            using (RestClient client = restClient)
+            {
+                var request = new RestRequest();
+                var response = client.Get(request);
+                return response;
+            }
+        }
+
+        private RestClient CreateClient(string url)
+        {
+            var options = new RestClientOptions(url);
+            var client = new RestClient(options);
+            return client;
+        }
+    }
+}
---
+++ ListDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using DemoClient;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Dynamic;
+using System.Text.Json.Serialization;
+
+namespace ListDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's get ourself properly insulted");
+            List<RestResponse> rawInsults = new List<RestResponse>();
+            ClientService clientService = new ClientService();
+            for (int i = 0; i < 5; i++) 
+            {
+                var response = clientService.GetInsult();
+                
+                rawInsults.Add(response);  
+            }
+            
+            
+            List<string> textInsults = DeserializeResponses(rawInsults);
+            foreach(var text in textInsults)
+            {
+                Console.WriteLine(text);
+            }
+            Console.WriteLine("Press any Key");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Look at List<T> docs at https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=net-7.0
+        /// </summary>
+        /// <param name="rawInsults"></param>
+        /// <returns></returns>
+        private static List<string> DeserializeResponses(List<RestResponse> rawInsults)
+        {
+            List<string> insultsOnly = new List<string>();
+            foreach (RestResponse response in rawInsults)
+            {
+                if (response.IsSuccessful)
+                {
+                    ResponseContent insult = JsonConvert.DeserializeObject<ResponseContent>(response.Content);
+                    insultsOnly.Add(insult.Insult);
+                }
+            }
+            int number = rawInsults.Count;
+            rawInsults.Clear(); //Well you can guess
+            number = rawInsults.Count;
+            insultsOnly.ForEach(insult => { Console.WriteLine(insult); }); //returns void, so do whatever necessary in the lambda-function
+            insultsOnly.Reverse();
+            insultsOnly.ForEach((insult) => { Console.WriteLine(insult); });
+            int capacity= insultsOnly.Capacity; // amount of space available before rezising list
+            
+            var readOnly = insultsOnly.AsReadOnly();
+            Console.WriteLine($"What's the type of the readOnly-object? {readOnly.GetType()}");
+            
+            return insultsOnly;
+        }
+
+        public class ResponseContent
+        {
+            string Number { get; set; }
+            string Language { get; set; }   
+            public string Insult { get; set; }
+            string Created { get; set; }
+            string Shown { get; set; }
+            string CreatedBy { get; set; }
+            string Active { get; set; } 
+            string Comment { get; set; }
+        }
+    }
+}
